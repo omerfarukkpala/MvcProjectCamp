1bc36d4b1339486096a5192e35c911482acb7088
= <omerfarukkpala@gmail.com>
2024-01-12 18:03:11 +0300

    enable-migrations true

--- DataAccessLayer/Migrations/Configuration.cs
+++ DataAccessLayer/Migrations/Configuration.cs
@@ -9,7 +9,7 @@
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(DataAccessLayer.Concrete.Context context)
