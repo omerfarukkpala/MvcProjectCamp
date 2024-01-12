520861eecbaeeeb381c7d70816604c196a97aa58
= <omerfarukkpala@gmail.com>
2024-01-12 11:53:53 +0300

    TestController

---
+++ MvcProjectCamp/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace A_MvcProKamp_UI.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: TestController 
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test2()
+        {
+            return View();
+        }
+
+        public ActionResult Test3()
+        {
+            return View();
+        }
+
+        public ActionResult SweetAlert()
+        {
+            return View();
+        }
+    }
+}
