cb57666f7304c9572d3fa45429782c72f771aaaf
= <omerfarukkpala@gmail.com>
2024-01-12 16:42:39 +0300

    ..

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        // SQL Tarafına Tabloları Yazdırmak İçin
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<ImageFile> ImageFiles { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+    }
+}
---
+++ DataAccessLayer/Concrete/GenericRepositoryDAL.cs
@@ -0,0 +1,62 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class GenericRepositoryDAL<T> : IRepositoryDAL<T> where T : class
+    {
+        Context db = new Context(); // sql bağlantı
+        DbSet<T> _object; // obje tanımladık
+
+        public GenericRepositoryDAL() // değer atamak için
+        {
+            _object = db.Set<T>();
+        }
+        public void Delete(T t)
+        {
+            var deletedEntity = db.Entry(t); // 1. yol
+            deletedEntity.State = EntityState.Deleted;
+
+          //  _object.Remove(t); 2. yol
+            db.SaveChanges();
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            return _object.SingleOrDefault(filter);
+        }
+
+        public void Insert(T t)
+        {
+            var addedEntity = db.Entry(t); // 1. yol
+            addedEntity.State = EntityState.Added;
+
+           // _object.Add(t); 2. yol
+            db.SaveChanges();
+        }
+
+        public List<T> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(T t)
+        {
+            var updatedEntity = db.Entry(t); // 1. yol
+            updatedEntity.State = EntityState.Modified;
+            
+            db.SaveChanges(); // 2. yol
+        }
+    }
+}
