96b2615b34ea4adaa784dd2aa62d6ded05d17ef2
= <omerfarukkpala@gmail.com>
2024-01-27 13:03:06 +0300

    ..

--- MvcProjectCamp/Controllers/CategoryController.cs
+++ MvcProjectCamp/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,17 @@ namespace MvcProjectCamp.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManagerBL cm = new CategoryManagerBL(new EfCategoryDAL());
+
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+
     }
 }
