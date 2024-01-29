a3d192ec0323659844c99611216874f9febf8436
= <omerfarukkpala@gmail.com>
2024-01-29 18:42:08 +0300

    ..

--- MvcProjectCamp/Controllers/CategoryController.cs
+++ MvcProjectCamp/Controllers/CategoryController.cs
@@ -6,7 +6,7 @@ using EntityLayer.Concrete;
 using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
