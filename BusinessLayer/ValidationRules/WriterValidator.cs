a302e4d2316ceb9747de3e375dca17fd6a5f3a18
= <omerfarukkpala@gmail.com>
2024-01-29 17:48:21 +0300

    ..

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adını boş geçemezsiniz");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar soy adını boş geçemezsiniz");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkımda kısmını boş geçemezsiniz");
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Unvan kısmını boş geçemezsiniz.");
+            //Ödev: Yazarın hakkında kısmında mutlaka A harfi ifade yer alsın.
+            //RuleFor(x => x.WriterAbout).Must(x => x != null && x.ToUpper().Contains("A")).WithMessage("Hakkında kısmında en az bir a harfi içermelidir");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapın");
+            RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("Lütfen 50 karakterden fazla değer girişi" +
+                "yapmayın");           
+        }
+    }
+}
