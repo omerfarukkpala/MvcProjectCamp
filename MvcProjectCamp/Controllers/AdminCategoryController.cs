e8b887c1a4a78b640763b94277af70c64b54687d
= <omerfarukkpala@gmail.com>
2024-01-31 12:18:38 +0300

    ..

--- MvcProjectCamp/Controllers/AdminCategoryController.cs
+++ MvcProjectCamp/Controllers/AdminCategoryController.cs
@@ -8,7 +8,6 @@ namespace MvcProjectCamp.Controllers
 {
     public class AdminCategoryController : Controller
     {
-        // GET: AdminCategory
         public ActionResult Index()
         {
             return View();
