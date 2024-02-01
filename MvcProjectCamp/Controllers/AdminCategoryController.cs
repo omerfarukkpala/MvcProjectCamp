1f28d72e8f2e4fa650d2d2ff0f23e5727c42ace9
= <omerfarukkpala@gmail.com>
2024-02-01 11:14:16 +0300

    ..

--- MvcProjectCamp/Controllers/AdminCategoryController.cs
+++ MvcProjectCamp/Controllers/AdminCategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +19,53 @@ namespace MvcProjectCamp.Controllers
             var categoryvalues = cm.GetList();
             return View(categoryvalues);
         }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            var categoryvalue = cm.GetByID(id);
+
+            cm.CategoryDelete(categoryvalue);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditCategory(int id)
+        {
+            var categoryvalue = cm.GetByID(id);
+            return View(categoryvalue);
+        }
+
+        [HttpPost]
+        public ActionResult EditCategory(Category p)
+        {
+            cm.CategoryUpdate(p);
+            return RedirectToAction("Index");
+        }
     }
 }
