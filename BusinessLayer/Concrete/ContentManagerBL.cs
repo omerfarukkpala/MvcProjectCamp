d01d02d0fec0e23dfc8d9e3b649fefed4979f4c7
= <omerfarukkpala@gmail.com>
2024-02-08 23:38:44 +0300

    .

--- BusinessLayer/Concrete/ContentManagerBL.cs
+++ BusinessLayer/Concrete/ContentManagerBL.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    //ContentManagerBL    
+    //ContentManagerBL     
     public class ContentManagerBL : IContentServiceBL
     {
         IContentDAL _contentDal;
