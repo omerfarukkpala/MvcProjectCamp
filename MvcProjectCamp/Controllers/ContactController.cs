cad0e2e19e13355773838cf46ae5c3e92f5e76bf
= <omerfarukkpala@gmail.com>
2024-02-09 11:55:56 +0300

    ..

--- MvcProjectCamp/Controllers/ContactController.cs
+++ MvcProjectCamp/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +12,9 @@ namespace MvcProjectCamp.Controllers
     public class ContactController : Controller
     {
         // GET: Contact
+        ContactManagerBL cm = new ContactManagerBL(new EfContactDAL());
+        MessageManagerBL mm = new MessageManagerBL(new EfMessageDAL());
+        ContactValidator cv = new ContactValidator();
         public ActionResult Index()
         {
             return View();
