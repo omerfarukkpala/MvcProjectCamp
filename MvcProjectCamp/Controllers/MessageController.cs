f2bab217c5268eefb17bdb0fa5d8aea1b5f4a0bc
= <omerfarukkpala@gmail.com>
2024-02-15 22:03:21 +0300

    ...

---
+++ MvcProjectCamp/Controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
