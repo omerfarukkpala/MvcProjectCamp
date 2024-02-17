e7dbfc8a7dd72f79d8e20ba851fdb6c7ca824610
= <omerfarukkpala@gmail.com>
2024-02-17 15:31:22 +0300

    ...        ImageFileManagerBL ifm = new ImageFileManagerBL(new EfImageFileDAL());

---
+++ MvcProjectCamp/Controllers/GalleryController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        ImageFileManagerBL ifm = new ImageFileManagerBL(new EfImageFileDAL());
+
+        public ActionResult Index()
+        {
+            var files = ifm.GetList();
+            return View(files);
+        }
+    }
+}
