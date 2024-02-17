b13d26f17a2c49a88267a23f6aa25690cf977c1f
= <omerfarukkpala@gmail.com>
2024-02-17 13:49:59 +0300

    ErrorPageController

---
+++ MvcProjectCamp/Controllers/ErrorPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        // GET: ErrorPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
