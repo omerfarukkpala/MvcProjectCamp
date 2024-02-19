80a80cc83230cc7b8b29fe168b521e1fc5db2f9d
= <omerfarukkpala@gmail.com>
2024-02-19 17:31:22 +0300

    ..

--- MvcProjectCamp/Controllers/WriterPanelContentController.cs
+++ MvcProjectCamp/Controllers/WriterPanelContentController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +13,36 @@ namespace MvcProjectCamp.Controllers
     public class WriterPanelContentController : Controller
     {
         // GET: WriterPanelContent
-        public ActionResult Index()
+        ContentManagerBL cm = new ContentManagerBL(new EfContentDAL());
+        Context c = new Context();
+        public ActionResult MyContent(string p)
+        {
+            p = (string)Session["WriterMail"];
+            var writeridinfo = c.Writers.Where(x => x.WriterMail == p).Select(y => y.WriterID).FirstOrDefault();
+            var contentvalues = cm.GetListByWriter(writeridinfo);
+            return View(contentvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddContent(int id)
+        {
+            ViewBag.d = id;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddContent(Content p)
+        {
+            string mail = (string)Session["WriterMail"];
+            var writeridinfo = c.Writers.Where(x => x.WriterMail == mail).Select(y => y.WriterID).FirstOrDefault();
+            p.ContentDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.WriterID = writeridinfo;
+            p.ContentStatus = true;
+            cm.ContentAdd(p);
+            return RedirectToAction("MyContent");
+        }
+
+        public ActionResult ToDoList()
         {
             return View();
         }
