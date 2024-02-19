cc42feef1e532ac42d6d5498612e172845842d04
= <omerfarukkpala@gmail.com>
2024-02-19 14:12:39 +0300

    .

--- MvcProjectCamp/Controllers/WriterPanelController.cs
+++ MvcProjectCamp/Controllers/WriterPanelController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +12,13 @@ namespace MvcProjectCamp.Controllers
     public class WriterPanelController : Controller
     {
         // GET: WriterPanel
+        // GET: WriterPanel
+        HeadingManagerBL hm = new HeadingManagerBL(new EfHeadingDAL());
+        CategoryManagerBL cm = new CategoryManagerBL(new EfCategoryDAL());
+        WriterManagerBL wm = new WriterManagerBL(new EfWriterDAL());
+        WriterValidator writerValidator = new WriterValidator();
+        Context c = new Context();
+
         public ActionResult Index()
         {
             return View();
