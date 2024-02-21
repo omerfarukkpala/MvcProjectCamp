bf725ac42b4a5373d590da8a1be6fc47b93b633f
= <omerfarukkpala@gmail.com>
2024-02-21 10:32:53 +0300

    ..

--- MvcProjectCamp/Controllers/DefaultController.cs
+++ MvcProjectCamp/Controllers/DefaultController.cs
@@ -13,9 +13,15 @@ namespace MvcProjectCamp.Controllers
         // GET: Default
         HeadingManagerBL hm = new HeadingManagerBL(new EfHeadingDAL());
         ContentManagerBL cm = new ContentManagerBL(new EfContentDAL());
-        public ActionResult Index()
+        public ActionResult Headings()
         {
-            return View();
+            var headinglist = hm.GetList();
+            return View(headinglist);
+        }
+        public PartialViewResult Index(int id = 0)
+        {
+            var contentlist = cm.GetListByHeadingID(id);
+            return PartialView(contentlist);
         }
     }
 }
