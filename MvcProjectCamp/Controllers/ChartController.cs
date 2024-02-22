eb50f5302416bd845cf39d002999d8db5f7a18c9
= <omerfarukkpala@gmail.com>
2024-02-22 21:26:26 +0300

    ..

--- MvcProjectCamp/Controllers/ChartController.cs
+++ MvcProjectCamp/Controllers/ChartController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcProjectCamp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,36 @@ namespace MvcProjectCamp.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult CategoryChart()
+        {
+            return Json(BlogList(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<CategoryClass> BlogList()
+        {
+            List<CategoryClass> ct = new List<CategoryClass>();
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Yazılım",
+                CategoryCount = 8
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Seyahat",
+                CategoryCount = 4
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Teknoloji",
+                CategoryCount = 7
+            });
+            ct.Add(new CategoryClass()
+            {
+                CategoryName = "Spor",
+                CategoryCount = 1
+            });
+            return ct;
         }
     }
 }
