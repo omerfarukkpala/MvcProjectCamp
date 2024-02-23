b0d01423c8b3268a796bb5d317f2a2c35bf9aa52
= <omerfarukkpala@gmail.com>
2024-02-23 10:50:06 +0300

    ..

---
+++ MvcProjectCamp/Controllers/StatisticsController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: Statistics
+        CategoryManagerBL cm = new CategoryManagerBL(new EfCategoryDAL());
+        Context db = new Context();
+        Statistics stats = new Statistics();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
