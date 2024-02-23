b8407e5e62dd4e3fdfcf4c0710f135cea595e9ad
= <omerfarukkpala@gmail.com>
2024-02-23 10:46:26 +0300

    ..

---
+++ EntityLayer/StaticHash.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class StaticHash
+    {
+        public static string hash = "";
+        public static string Encrypt(string text)
+        {
+            byte[] data = Encoding.Default.GetBytes(text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(Encoding.Default.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripleDES.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    return Convert.ToBase64String(results, 0, results.Length);
+                }
+            }
+        }
+
+        public static string Descrypt(string text)
+        {
+            byte[] data = Convert.FromBase64String(text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(Encoding.Default.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripleDES.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    return Encoding.Default.GetString(results);
+                }
+            }
+        }
+    }
+}
---
+++ EntityLayer/StaticModel.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class StaticModel
+    {
+        public int InboxCount { get; set; }
+        public int SendboxCount { get; set; }
+        public int ReadSendboxCount { get; set; }
+        public int UnreadableSendboxCount { get; set; }
+    }
+
+    public class CaptchaResponse
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        [JsonProperty("error-codes")]
+        public List<string> ErrorCodes { get; set; }
+    }
+
+    public class Calender
+    {
+        public String title { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+        public bool allDay { get; set; }
+    }
+}
