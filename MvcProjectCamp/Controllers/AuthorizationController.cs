c9bf5f8b0e5f1d9a693f9b5f2b7619a23cbf9d33
= <omerfarukkpala@gmail.com>
2024-02-23 09:19:15 +0300

    ..

--- MvcProjectCamp/Controllers/AuthorizationController.cs
+++ MvcProjectCamp/Controllers/AuthorizationController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,8 @@ namespace MvcProjectCamp.Controllers
 {
     public class AuthorizationController : Controller
     {
+        AdminManagerBL adminmanager = new AdminManagerBL(new EfAdminDAL());
+
         // GET: Authorization
         public ActionResult Index()
         {
