d405740c904b1887deb382cdfe7e7bab80e4e831
= <omerfarukkpala@gmail.com>
2024-02-23 09:19:27 +0300

    .

--- MvcProjectCamp/Controllers/AuthorizationController.cs
+++ MvcProjectCamp/Controllers/AuthorizationController.cs
@@ -1,10 +1,12 @@
-﻿using BusinessLayer.Concrete;
-using DataAccessLayer.EntityFramework;
+﻿using EntityLayer.Concrete;
+using EntityLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 
 namespace MvcProjectCamp.Controllers
 {
