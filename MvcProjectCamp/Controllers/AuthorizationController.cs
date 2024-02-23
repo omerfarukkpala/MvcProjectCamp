722459fdda37a40627a974c0903511d36154b1f8
= <omerfarukkpala@gmail.com>
2024-02-23 09:18:28 +0300

    ..

--- MvcProjectCamp/Controllers/AuthorizationController.cs
+++ MvcProjectCamp/Controllers/AuthorizationController.cs
@@ -13,5 +13,75 @@ namespace MvcProjectCamp.Controllers
         {
             return View();
         }
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            List<SelectListItem> valueAdminRole = new List<SelectListItem>();
+            valueAdminRole.Add(new SelectListItem
+            {
+                Text = "A",
+                Value = "A"
+
+            });
+            valueAdminRole.Add(new SelectListItem
+            {
+                Text = "B",
+                Value = "B"
+
+            });
+            valueAdminRole.Add(new SelectListItem
+            {
+                Text = "C",
+                Value = "C"
+
+            });
+            ViewBag.vlc = valueAdminRole;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddAdmin(Admin p)
+        {
+            string encUsername = StaticHash.Encrypt(p.AdminUserName);
+            string encPassword = StaticHash.Encrypt(p.AdminPassword);
+            p.AdminUserName = encUsername;
+            p.AdminPassword = encPassword;
+            adminmanager.AdminAdd(p);
+            return RedirectToAction("AddAdmin");
+        }
+        [HttpGet]
+        public ActionResult EditAdmin(int id)
+        {
+            List<SelectListItem> valueAdminRole = new List<SelectListItem>();
+            valueAdminRole.Add(new SelectListItem
+            {
+                Text = "A",
+                Value = "A"
+
+            });
+            valueAdminRole.Add(new SelectListItem
+            {
+                Text = "B",
+                Value = "B"
+
+            });
+            valueAdminRole.Add(new SelectListItem
+            {
+                Text = "C",
+                Value = "C"
+
+            });
+            ViewBag.vlc = valueAdminRole;
+
+            var adminValue = adminmanager.GetByID(id);
+            return View(adminValue);
+        }
+
+        [HttpPost]
+        public ActionResult EditAdmin(Admin p)
+        {
+            adminmanager.AdminUpdate(p);
+            return RedirectToAction("Index");
+        }
     }
 }
