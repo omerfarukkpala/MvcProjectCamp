cbd16618918f69f16b71c8ab8c9e2c21f9f3d6c1
= <omerfarukkpala@gmail.com>
2024-02-23 10:50:50 +0300

    .

--- MvcProjectCamp/Controllers/StatisticsController.cs
+++ MvcProjectCamp/Controllers/StatisticsController.cs
@@ -18,7 +18,25 @@ namespace MvcProjectCamp.Controllers
         Statistics stats = new Statistics();
         public ActionResult Index()
         {
-            return View();
+            //1. Soru: Toplam Kategori Sayısı
+            stats.TotalCategoryCount = cm.GetList().Count();
+
+            //2. Soru: Başlık tablosunda "yazılım" kategorisine ait başlık sayısı
+            stats.TotalSoftwareCategoryCount = db.Headings.Count(x => x.Category.CategoryName == "Yazılım");
+
+            //3. Soru: Yazar adında 'a' harfi geçen yazar sayısı
+            stats.TotalWriterLetterACount = db.Writers.Count(x => x.WriterName.ToLower().Contains("a"));
+
+            //4. Soru: En fazla başlığa sahip kategori adı
+            stats.MaxHeadingCategoryName = cm.GetList().Where(x => x.CategoryID == db.Headings.ToList()
+            .GroupBy(y => y.CategoryID).ToList().OrderBy(z => z.Count()).Last().Key).FirstOrDefault().CategoryName;
+
+            //5. Soru: Kategori tablosunda durumu true olan kategoriler ile false olan kategoriler arasındaki
+            //sayısal fark
+            stats.DifferenceCategoryStatus = cm.GetList().Where(x => x.CategoryStatus == true).Count() -
+                cm.GetList().Where(x => x.CategoryStatus == false).Count();
+
+            return View(stats);
         }
     }
 }
