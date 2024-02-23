a56ffc05c2e5b38a031388d1f0f5c69b6fce2608
= <omerfarukkpala@gmail.com>
2024-02-23 09:17:51 +0300

    AuthorizationController

---
+++ MvcProjectCamp/Controllers/AuthorizationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        // GET: Authorization
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
