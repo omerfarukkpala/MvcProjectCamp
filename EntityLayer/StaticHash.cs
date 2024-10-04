ddd91d0708f8f63d4ee0ee8bb8b75f55e4151eff
Ömer Faruk PALA <101570820+omerfarukkpala@users.noreply.github.com>
2024-10-04 14:26:14 +0300

    Update StaticHash.cs
    EntityLayer

--- EntityLayer/StaticHash.cs
+++ EntityLayer/StaticHash.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
 
 namespace EntityLayer
 {
